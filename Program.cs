26b474ec4e39d47479c70595cc01a9fcf9fa2ad1
joshu <joshu@REEDPC>
2024-05-16 15:05:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment2._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "personalDetails";
+            File.WriteAllText("details.txt", writeText);
+
+            string readText = File.ReadAllText("details.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
